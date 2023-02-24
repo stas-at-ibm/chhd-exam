9c0938ff731bca06d385d740bb4c2107449b3533
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-02-24 11:55:09 +0100

    added main boilerplate

---
+++ src/main.ts
@@ -0,0 +1,133 @@
+import { log } from "./utils/log";
+import { AccountUseCases } from "./use-cases/accounts.use.cases";
+import { TokenUseCase } from "./use-cases/token.use.cases";
+import { ScheduledUseCases } from "./use-cases/scheduled.use.cases";
+import { MultiSigUseCases } from "./use-cases/multi.sig.use.cases";
+import { ConsensusUseCases } from "./use-cases/consensus.use.cases";
+import { SmartContractUseCases } from "./use-cases/smart.contract.use.cases";
+
+// ! ACCOUNT USE CASES
+// AccountUseCases.createAccounts()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// ! TOKEN USE CASES
+// TokenUseCase.addNameCase1()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// TokenUseCase.addNameCase2()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// ! SCHEDULE USE CASES
+// ScheduledUseCases.scheduleTx()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// ScheduledUseCases.deleteTx()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// ! MULTISIG USE CASES
+// MultiSigUseCases.addNameCase1()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// MultiSigUseCases.addNameCase2()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// ! CONSENSUS USE CASES
+// ConsensusUseCases.addNameCase1()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// ConsensusUseCases.addNameCase2()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// ! SMART CONTRACT USE CASES
+// SmartContractUseCases.deploySmartContract()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// SmartContractUseCases.callFunc1()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
+
+// SmartContractUseCases.deleteContract()
+//   .then(() => {
+//     log.info("done...");
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     log.error(err);
+//     process.exit();
+//   });
