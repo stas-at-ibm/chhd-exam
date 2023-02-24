321736841c854b52bd000abbe2760ae111a5ab2d
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-02-24 13:26:24 +0100

    added account creation

--- src/use-cases/accounts.use.cases.ts
+++ src/use-cases/accounts.use.cases.ts
@@ -16,5 +16,65 @@ interface Account {
 }
 
 export class AccountUseCases {
-  static async createAccounts() {}
+  static async createAccounts() {
+    const accounts = await Promise.all(
+      ["account1", "account2", "account3", "account4", "account5"].map(
+        async (name) => await createOneAccount(name, 1000),
+      ),
+    );
+
+    for (const acc of accounts) {
+      await logAccountInfo(acc);
+    }
+
+    for (const acc of accounts) {
+      persistAccountToFs(acc);
+    }
+
+    async function createOneAccount(name: string, initHbar: number): Promise<Account> {
+      const newPrivateKey = PrivateKey.generateED25519();
+      const newPublicKey = newPrivateKey.publicKey;
+
+      const txResponse = await new AccountCreateTransaction()
+        .setKey(newPublicKey)
+        .setInitialBalance(new Hbar(initHbar))
+        .execute(client);
+
+      const receipt = await txResponse.getReceipt(client);
+
+      log.info(`transaction status is ${receipt.status}`);
+
+      const newAccountId = receipt.accountId!;
+
+      return {
+        name,
+        id: newAccountId.toString(),
+        privateKey: newPrivateKey.toStringRaw(),
+        publicKey: newPublicKey.toStringRaw(),
+      };
+    }
+
+    async function logAccountInfo(acc: Account) {
+      const balance = await new AccountBalanceQuery()
+        .setAccountId(acc.id)
+        .execute(client);
+
+      log.info(`the new account name is: ${acc.name}`);
+      log.info(`the new account ID is: ${acc.id}`);
+      log.info(`the new account private key is: ${acc.privateKey}`);
+      log.info(`the new account public key is: ${acc.publicKey}`);
+      log.info(`the new account balance is: ${balance.hbars}\n`);
+    }
+
+    function persistAccountToFs(acc: Account) {
+      const path = `${__dirname.replace("/src/use-cases", "")}/artifacts/${
+        acc.name
+      }.json`;
+      fs.writeFileSync(path, JSON.stringify(acc, null, 2));
+
+      log.info(
+        `persisted account ${acc.name} to path: <project-root>/artifacts/${acc.name}.json`,
+      );
+    }
+  }
 }
