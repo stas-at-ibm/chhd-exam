3a766ca2853f2d0cb00ac3d7e359ac5e6ea4a2b5
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-02-24 15:52:07 +0100

    impl smart conract use cases

--- src/use-cases/smart.contract.use.cases.ts
+++ src/use-cases/smart.contract.use.cases.ts
@@ -10,32 +10,52 @@ import { log } from "src/utils/log";
 
 export class SmartContractUseCases {
   static async deploySmartContract() {
-    // const bytecode = require("../../artifacts/ADD_FILE_NAME.json").bytecode;
-    //
-    // tx
-    //
-    // const signTx = await tx.sign(ADD_PRIVATE_KEY);
-    // const txResponse = await signTx.execute(client);
-    // const receipt = await txResponse.getReceipt(client);
-    // log.info(`transaction status is ${receipt.status}`);
-    // log.info(`your contract ID is: ${receipt.contractId}`);
+    const bytecode = require("../../artifacts/CertificationC1.json").bytecode;
+
+    const tx = new ContractCreateFlow()
+      .setGas(100000)
+      .setBytecode(bytecode)
+      .setAdminKey(env.acc1.publicKey);
+
+    const signTx = await tx.sign(env.acc1.privateKey);
+
+    const txResponse = await signTx.execute(client);
+
+    const receipt = await txResponse.getReceipt(client);
+
+    log.info(`transaction status is ${receipt.status}`);
+
+    log.info(`your contract ID is: ${receipt.contractId}`);
   }
 
   static async callFunc1() {
-    // const tx = new ContractExecuteTransaction()
-    //
-    // const signTx = await tx.sign(ADD KAY);
-    // const txResponse = await signTx.execute(client);
-    // const receipt = await txResponse.getReceipt(client);
-    // log.info(`transaction status is ${receipt.status}`);
-    // const record = await txResponse.getRecord(client);
-    //
-    // log.info(`the result of ADD FUNC NAME is: ${record.contractFunctionResult?.ADD METHOD}`);
+    const tx = new ContractExecuteTransaction()
+      .setContractId(env.contractId)
+      .setGas(100000)
+      .setFunction(
+        "function1",
+        new ContractFunctionParameters().addUint16(6).addUint16(7),
+      )
+      .freezeWith(client);
+
+    const signTx = await tx.sign(env.acc1.privateKey);
+
+    const txResponse = await signTx.execute(client);
+
+    const receipt = await txResponse.getReceipt(client);
+
+    log.info(`transaction status is ${receipt.status}`);
+
+    const record = await txResponse.getRecord(client);
+
+    log.info(`the result of function1 is: ${record.contractFunctionResult?.getUint32()}`);
   }
 
   static async deleteContract() {
     // const tx = await new ContractDeleteTransaction()
-    //
+    //   .setContractId(env.contractId)
+    //   .setTransferAccountId(env.mainAcc.id)
+    //   .freezeWith(client);
     // const signTx = await tx.sign(env.acc1.privateKey);
     // const txResponse = await signTx.execute(client);
     // const receipt = await txResponse.getReceipt(client);
